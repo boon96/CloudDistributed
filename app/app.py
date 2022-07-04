365f3d3af063ff5dd0bdf3f4da57572163cf7341
boon96 <boonchungoh@gmail.com>
2022-07-04 16:04:34 +0800

    some minor changes

--- app/app.py
+++ app/app.py
@@ -1,15 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Nov 17 21:40:41 2020
-
-@author: win10
-"""
 
 # 1. Library imports
 import uvicorn
 from fastapi import FastAPI
 from BankNotes import BankNote
-import numpy as np
 import pickle
 import pandas as pd
 # 2. Create the app object
