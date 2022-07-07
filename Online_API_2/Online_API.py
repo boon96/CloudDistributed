5f6fafe157913b87788fe62c62f10617c11e25f9
leelin245 <leelin245@users.noreply.github.com>
2022-07-07 23:01:23 +0800

    api2

---
+++ Online_API_2/Online_API.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+import cv2
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return{"Hello": "World"}
+
+# Request to predict
+@app.get("/predict/{image}")
+def prediction(image):
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+    img = cv2.imread(f'API_Images/{image}')
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    if len(faces) == 0:
+        print("No Face")
+        return {"result" : "Not Face"}
+    else:
+        print("Got Face")
+        return {"result" : "Face"}
+
